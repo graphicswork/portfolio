e593a2df4622a9544179f8e92e62f2185403eeca
Chirag <chirag9376@gmail.com>
2019-07-07 15:17:46 +0530

    added new design
    added new design update in sub folder

---
+++ new/js/script.js
@@ -0,0 +1,37 @@
+  $(document).ready(function(){
+    $("body").addClass('home');
+    var typed = new Typed(".element", {
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 2000,
+      startDelay: 500,
+      //smartBackspace:false,
+      loop: true,
+      showCursor: true,
+      strings: ["Designing Logo", "Creating Layouts"]
+    });
+    
+    $(".menu a ").click(function () {
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top - 90
+    }, 600);
+    return false;
+  });
+  /*  $('.testmonials').owlCarousel({
+      items:1,
+      loop:true,
+      margin:0,
+      nav:true,
+      autoplay:true,
+      smartSpeed:1000,
+      autoplayTimeout:9000,
+      responsive:{
+        0:{
+          items:1
+        },
+        768:{
+          items:2
+        }
+      }
+    }); */    
+  });
