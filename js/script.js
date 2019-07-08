af00ced9a08c738b735aa42c3b50952d33290761
Chirag <chirag9376@gmail.com>
2019-07-08 23:09:17 +0530

    updated script

--- js/script.js
+++ js/script.js
@@ -14,27 +14,3 @@ function sizeContent() {
 $(document).ready(sizeContent);
 $(window).resize(sizeContent);
 
-$(document).ready(function() {
-  //$("[data-fancybox]").fancybox({clickSlide : false});
-  $("[data-fancybox]").fancybox();
-
-  $(function() {
-      $("#menu-trigger").click(function() {
-          $(".leftbar").toggleClass('mobimenu');
-      });
-  });
-
-  function sizeContent() {
-      var windowWidth = $(window).width();
-      if (windowWidth > 767) {
-          $(".leftbar").removeClass('mobimenu');
-          $(".menu > li").find("ul").removeAttr("style");
-      }
-  };
-  $(document).ready(sizeContent);
-  $(window).resize(sizeContent);
-
-  //$("[data-fancybox]").fancybox({clickSlide : false});
-  $("[data-fancybox]").fancybox();   
-
-});
