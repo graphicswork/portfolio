3c30b7e909cb1531632fc9d5cae1e852be330039
Chirag <chirag9376@gmail.com>
2019-07-13 00:14:09 +0530

    Update JS for menu issue

--- js/script.js
+++ js/script.js
@@ -1,44 +1,20 @@
-/*$(window).scroll(function(){
-  if ($(window).scrollTop() >= 80) {
-    $('.header').addClass('fixed');
-  } else {
-    $('.header').removeClass('fixed');
-  };
-}); 
-*/
 $(function(){
   $("#menu-trigger").click(function(){
-    $(".header").toggleClass('mobimenu');
-    mobiMenu();
+    $(".leftbar").toggleClass('mobimenu');
   });
 });
 
 function sizeContent() {
   var windowWidth = $(window).width();
   if (windowWidth > 767) {
-    $(".header").removeClass('mobimenu');
+    $(".leftbar").removeClass('mobimenu');
     $(".menu > li" ).find( "ul" ).removeAttr("style");
-  };
+  }
 };
 $(document).ready(sizeContent);
 $(window).resize(sizeContent);
 
-function mobiMenu() {
-  $(".mobimenu .menu > li" ).find( "ul" ).parent("li").addClass("color" );
-  $(".mobimenu .menu li.color > a").append('<span class="arrow"></span>');
-  $('.mobimenu .menu li.color > a .arrow').closest('li').removeClass("open" );
-  $('.mobimenu .menu li.color > a .arrow').click(function(e) {
-    e.preventDefault();
-    $(this).closest('li').toggleClass("open" ).siblings().removeClass("open" );
-    $(this).closest('.menu').siblings().find('li').removeClass("open" );
-    $(this).closest('li').siblings().find('> ul').slideUp(300);
-    $(this).closest('.menu').siblings().find('li > ul').slideUp(300);
-    $(this).closest('li').find('> ul').slideToggle(300);
-  })
-};
-
-
-$(document).ready(function () {
-  mobiMenu();
-  $('[data-fancybox]').fancybox();  
+$(document).ready(function(){
+  //$("[data-fancybox]").fancybox({clickSlide : false});
+ $("[data-fancybox]").fancybox();
 });
