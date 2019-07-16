4f35f654fdd293471295174fb02a4584b3d874f3
Chirag <chirag9376@gmail.com>
2019-07-16 23:22:38 +0530

    update page, add graphic in portfolio

--- new/js/script.js
+++ new/js/script.js
@@ -1,80 +1,80 @@
 $(document).ready(function() {
   $("body").addClass('home');
   var typed = new Typed(".element", {
-      typeSpeed: 50,
-      backSpeed: 30,
-      backDelay: 2000,
-      startDelay: 500,
-      //smartBackspace:false,
-      loop: true,
-      showCursor: true,
-      strings: ["Designing Logo", "Creating Layouts"]
+   typeSpeed: 50,
+   backSpeed: 30,
+   backDelay: 2000,
+   startDelay: 500,
+   //smartBackspace:false,
+   loop: true,
+   showCursor: true,
+   strings: ["Designing Logo.", "Creating Layouts.", "Branding Material.", "Brand illustration."]
   });
   // for portfolio
   size_li = $(".portfolio > article").size();
   x = 4;
   $('.portfolio > article:lt(' + x + ')').show();
   $('#loadMore').click(function() {
-      x = (x + 4 <= size_li) ? x + 4 : size_li;
-      $('.portfolio > article:lt(' + x + ')').show('slow');
-      $('#showLess').show();
-      $('#viewall').show();
-      if (x == size_li) {
-          $('#loadMore').hide();
-      }
+   x = (x + 4 <= size_li) ? x + 4 : size_li;
+   $('.portfolio > article:lt(' + x + ')').show('slow');
+   $('#showLess').show();
+   $('#viewall').show();
+   if (x == size_li) {
+    $('#loadMore').hide();
+   }
   });
   $('#showLess').click(function() {
-      x = (x - 4 < 0) ? 3 : x - 4;
-      $('.portfolio > article').not(':lt(' + x + ')').hide('slow');
-      $('#loadMore').show();
-      $('#showLess').show();
-      $('#viewall').show();
-      if (x == 4) {
-          $('#showLess').hide();
-          $('#viewall').hide();
-      }
+   x = (x - 4 < 0) ? 3 : x - 4;
+   $('.portfolio > article').not(':lt(' + x + ')').hide('slow');
+   $('#loadMore').show();
+   $('#showLess').show();
+   $('#viewall').show();
+   if (x == 4) {
+    $('#showLess').hide();
+    $('#viewall').hide();
+   }
   });
-
-});
-
-$(window).scroll(function() {
+ 
+ });
+ 
+ $(window).scroll(function() {
   if ($(window).scrollTop() >= 100) {
-      $('.header').addClass('fixed');
-      $('.scrollup').addClass('up');
+   $('.header').addClass('fixed');
+   $('.scrollup').addClass('up');
   } else {
-      $('.header').removeClass('fixed');
-      $('.scrollup').removeClass('up');
+   $('.header').removeClass('fixed');
+   $('.scrollup').removeClass('up');
   };
-});
-
-
-$(function() {
+ });
+ 
+ 
+ $(function() {
   $("#menu-trigger").click(function() {
-      $(".header").toggleClass('mobimenu');
-    //  mobiMenu();
+   $(".header").toggleClass('mobimenu');
+   //  mobiMenu();
   });
-});
-
-function sizeContent() {
+ });
+ 
+ function sizeContent() {
   var windowWidth = $(window).width();
   if (windowWidth > 1023) {
-      $(".header").removeClass('mobimenu');
-      $(".menu > li").find("ul").removeAttr("style");
+   $(".header").removeClass('mobimenu');
+   $(".menu > li").find("ul").removeAttr("style");
   };
-};
-$(document).ready(sizeContent);
-$(window).resize(sizeContent);
-
-$(document).ready(function() {
- // mobiMenu();
+ };
+ $(document).ready(sizeContent);
+ $(window).resize(sizeContent);
+ 
+ $(document).ready(function() {
+  // mobiMenu();
   $('[data-fancybox]').fancybox();
   $.scrollIt({
-    upKey:38,
-    downKey:40,
-    easing:"linear",
-    scrollTime:600,
-    activeClass:"active",
-    onPageChange:null,
-    topOffset:-54
+   upKey: 38,
+   downKey: 40,
+   easing: "linear",
+   scrollTime: 600,
+   activeClass: "active",
+   onPageChange: null,
+   topOffset: -54
   });
-});
+ });
