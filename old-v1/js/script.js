583ad3d1b0bc645d676c44ec56716298b93e0641
Chirag <chirag9376@gmail.com>
2019-07-27 16:37:37 +0530

    move old portfolio and new make it live

---
+++ old-v1/js/script.js
@@ -0,0 +1,20 @@
+$(function(){
+  $("#menu-trigger").click(function(){
+    $(".leftbar").toggleClass('mobimenu');
+  });
+});
+
+function sizeContent() {
+  var windowWidth = $(window).width();
+  if (windowWidth > 767) {
+    $(".leftbar").removeClass('mobimenu');
+    $(".menu > li" ).find( "ul" ).removeAttr("style");
+  }
+};
+$(document).ready(sizeContent);
+$(window).resize(sizeContent);
+
+$(document).ready(function(){
+  //$("[data-fancybox]").fancybox({clickSlide : false});
+ $("[data-fancybox]").fancybox();
+});
